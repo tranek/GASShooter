0aa3831c28f3a8e1b4ce237a61597356be21e316
tranek <1534735+tranek@users.noreply.github.com>
2020-01-22 12:29:26 -0500

    Added Container function category

--- Source/GASShooter/Public/GSBlueprintFunctionLibrary.h
+++ Source/GASShooter/Public/GSBlueprintFunctionLibrary.h
@@ -26,17 +26,17 @@ public:
 	/**
 	* GameplayAbility
 	*/
-	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	UFUNCTION(BlueprintCallable, Category = "Ability")
 	static UGSGameplayAbility* GetPrimaryAbilityInstanceFromHandle(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAbilitySpecHandle Handle);
 
-	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	UFUNCTION(BlueprintCallable, Category = "Ability")
 	static UGSGameplayAbility* GetPrimaryAbilityInstanceFromClass(UAbilitySystemComponent* AbilitySystemComponent, TSubclassOf<UGameplayAbility> InAbilityClass);
 
 
 	/**
 	* FGameplayAbilitySpecHandle
 	*/
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Abilities")
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Ability")
 	static bool IsAbilitySpecHandleValid(FGameplayAbilitySpecHandle Handle);
 
 
@@ -46,22 +46,22 @@ public:
 
 	// USTRUCTs cannot contain UFUNCTIONS so we make static functions here
 	// Checks if spec has any effects
-	UFUNCTION(BlueprintPure, Category = "Ability")
+	UFUNCTION(BlueprintPure, Category = "Ability|Container")
 	static bool DoesEffectContainerSpecHaveEffects(const FGSGameplayEffectContainerSpec& ContainerSpec);
 
 	// Checks if spec has any targets
-	UFUNCTION(BlueprintPure, Category = "Ability")
+	UFUNCTION(BlueprintPure, Category = "Ability|Container")
 	static bool DoesEffectContainerSpecHaveTargets(const FGSGameplayEffectContainerSpec& ContainerSpec);
 
 	// Clears spec's targets
-	UFUNCTION(BlueprintCallable, Category = "Ability")
+	UFUNCTION(BlueprintCallable, Category = "Ability|Container")
 	static void ClearEffectContainerSpecTargets(UPARAM(ref) FGSGameplayEffectContainerSpec& ContainerSpec);
 
 	// Adds targets to a copy of the passed in effect container spec and returns it
-	UFUNCTION(BlueprintCallable, Category = "Ability", Meta = (AutoCreateRefTerm = "TargetData, HitResults, TargetActors"))
+	UFUNCTION(BlueprintCallable, Category = "Ability|Container", Meta = (AutoCreateRefTerm = "TargetData, HitResults, TargetActors"))
 	static void AddTargetsToEffectContainerSpec(UPARAM(ref) FGSGameplayEffectContainerSpec& ContainerSpec, const TArray<FGameplayAbilityTargetDataHandle>& TargetData, const TArray<FHitResult>& HitResults, const TArray<AActor*>& TargetActors);
 
 	// Applies container spec that was made from an ability
-	UFUNCTION(BlueprintCallable, Category = "Ability")
+	UFUNCTION(BlueprintCallable, Category = "Ability|Container")
 	static TArray<FActiveGameplayEffectHandle> ApplyExternalEffectContainerSpec(const FGSGameplayEffectContainerSpec& ContainerSpec);
 };
