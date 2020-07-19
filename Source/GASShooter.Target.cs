0e8c2903c74d46ca6a67b8809ffc2d47720c3a85
tranek <1534735+tranek@users.noreply.github.com>
2020-07-18 22:25:12 -0400

    Updated packaged build to use BuildSettingsVersion.V2

--- Source/GASShooter.Target.cs
+++ Source/GASShooter.Target.cs
@@ -8,7 +8,7 @@ public class GASShooterTarget : TargetRules
 	public GASShooterTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-
+		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "GASShooter" } );
 	}
 }
