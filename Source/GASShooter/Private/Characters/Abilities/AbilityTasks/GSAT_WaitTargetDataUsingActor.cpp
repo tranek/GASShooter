tranek/GASShooter

commit a9015929bc62d0777fba9ecdc1a62c041e687796

Upgrade to 5.0
Still some bugs. Not pushing to master branch until they're solved.

- UI_FloatingStatusBar_Hero Event Construct -> OwningCharacter is nullptr so it's not adding attribute changed listeners. AGSHeroCharacter::InitializeFloatingStatusBar() isn't being called.

- Weapons are only playing reload anim on simulated clients the first time after being equipped. Subsequent reload anims aren't playing.

Source/GASShooter/Private/Characters/Abilities/AbilityTasks/GSAT_WaitTargetDataUsingActor.cpp
@@ -21,7 +21,7 @@ UGSAT_WaitTargetDataUsingActor* UGSAT_WaitTargetDataUsingActor::WaitTargetDataWi
 
 void UGSAT_WaitTargetDataUsingActor::Activate()
 {
-	if (IsPendingKill())
+	if (!IsValid(this))
 	{
 		return;
 	}
@@ -205,7 +205,7 @@ void UGSAT_WaitTargetDataUsingActor::FinalizeTargetActor() const
 
 void UGSAT_WaitTargetDataUsingActor::RegisterTargetDataCallbacks()
 {
-	if (!ensure(IsPendingKill() == false))
+	if (!ensure(!IsValid(this)))
 	{
 		return;
 	}

Config/HoloLens/HoloLensEngine.ini
@@ -1,0 +1,31 @@
+
+
+[/Script/HoloLensPlatformEditor.HoloLensTargetSettings]
+bBuildForEmulation=False
+bBuildForDevice=True
+bUseNameForLogo=True
+bBuildForRetailWindowsStore=False
+bAutoIncrementVersion=False
+bShouldCreateAppInstaller=False
+AppInstallerInstallationURL=
+HoursBetweenUpdateChecks=0
+bEnablePIXProfiling=False
+TileBackgroundColor=(B=64,G=0,R=0,A=255)
+SplashScreenBackgroundColor=(B=64,G=0,R=0,A=255)
++PerCultureResources=(CultureId="",Strings=(PackageDisplayName="",PublisherDisplayName="",PackageDescription="",ApplicationDisplayName="",ApplicationDescription=""),Images=())
+TargetDeviceFamily=Windows.Holographic
+MinimumPlatformVersion=
+MaximumPlatformVersionTested=10.0.18362.0
+MaxTrianglesPerCubicMeter=500.000000
+SpatialMeshingVolumeSize=20.000000
+CompilerVersion=Default
+Windows10SDKVersion=10.0.18362.0
++CapabilityList=internetClientServer
++CapabilityList=privateNetworkClientServer
++Uap2CapabilityList=spatialPerception
+bSetDefaultCapabilities=False
+SpatializationPlugin=
+ReverbPlugin=
+OcclusionPlugin=
+SoundCueCookQualityIndex=-1
+

GASShooter.uproject
@@ -1,6 +1,6 @@
 {
 	"FileVersion": 3,
-	"EngineAssociation": "4.27",
+	"EngineAssociation": "5.0",
 	"Category": "",
 	"Description": "",
 	"Modules": [
@@ -36,6 +36,19 @@
 				"Mac",
 				"Win64"
 			]
+		},
+		{
+			"Name": "Bridge",
+			"Enabled": true,
+			"SupportedTargetPlatforms": [
+				"Win64",
+				"Mac",
+				"Linux"
+			]
+		},
+		{
+			"Name": "AndroidFileServer",
+			"Enabled": false
 		}
 	]
 }

Source/GASShooter/Private/Characters/Abilities/AbilityTasks/GSAT_WaitInteractableTarget.cpp
@@ -67,10 +67,10 @@ void UGSAT_WaitInteractableTarget::LineTrace(FHitResult& OutHitResult, const UWo
 	{
 		const FHitResult& Hit = HitResults[HitIdx];
 
-		if (!Hit.Actor.IsValid() || Hit.Actor != Ability->GetCurrentActorInfo()->AvatarActor.Get())
+		if (!Hit.GetActor() || Hit.GetActor() != Ability->GetCurrentActorInfo()->AvatarActor.Get())
 		{
 			// If bLookForInteractableActor is false, we're looking for an endpoint to trace to
-			if (bLookForInteractableActor && Hit.Actor.IsValid())
+			if (bLookForInteractableActor && Hit.GetActor())
 			{
 				// bLookForInteractableActor is true, hit component must overlap COLLISION_INTERACTABLE trace channel
 				// This is so that a big Actor like a computer can have a small interactable button.
@@ -78,9 +78,9 @@ void UGSAT_WaitInteractableTarget::LineTrace(FHitResult& OutHitResult, const UWo
 					== ECollisionResponse::ECR_Overlap)
 				{
 					// Component/Actor must be available to interact
-					bool bIsInteractable = Hit.Actor.Get()->Implements<UGSInteractable>();
+					bool bIsInteractable = Hit.GetActor()->Implements<UGSInteractable>();
 
-					if (bIsInteractable && IGSInteractable::Execute_IsAvailableForInteraction(Hit.Actor.Get(), Hit.Component.Get()))
+					if (bIsInteractable && IGSInteractable::Execute_IsAvailableForInteraction(Hit.GetActor(), Hit.Component.Get()))
 					{
 						OutHitResult = Hit;
 						OutHitResult.bBlockingHit = true; // treat it as a blocking hit
@@ -223,7 +223,7 @@ void UGSAT_WaitInteractableTarget::PerformTrace()
 		// No valid, available Interactable Actor
 
 		ReturnHitResult.Location = TraceEnd;
-		if (TargetData.Num() > 0 && TargetData.Get(0)->GetHitResult()->Actor.Get())
+		if (TargetData.Num() > 0 && TargetData.Get(0)->GetHitResult()->GetActor())
 		{
 			// Previous trace had a valid Interactable Actor, now we don't have one
 			// Broadcast last valid target
@@ -240,9 +240,9 @@ void UGSAT_WaitInteractableTarget::PerformTrace()
 
 		if (TargetData.Num() > 0)
 		{
-			const AActor* OldTarget = TargetData.Get(0)->GetHitResult()->Actor.Get();
+			const AActor* OldTarget = TargetData.Get(0)->GetHitResult()->GetActor();
 
-			if (OldTarget == ReturnHitResult.Actor.Get())
+			if (OldTarget == ReturnHitResult.GetActor())
 			{
 				// Old target is the same as the new target, don't broadcast the target
 				bBroadcastNewTarget = false;

Source/GASShooter/Private/Characters/Abilities/AsyncTaskAttributeChanged.cpp
@@ -53,7 +53,7 @@ void UAsyncTaskAttributeChanged::EndTask()
 	}
 
 	SetReadyToDestroy();
-	MarkPendingKill();
+	MarkAsGarbage();
 }
 
 void UAsyncTaskAttributeChanged::AttributeChanged(const FOnAttributeChangeData & Data)

Source/GASShooter/Private/Characters/Abilities/AsyncTaskGameplayTagAddedRemoved.cpp
@@ -41,7 +41,7 @@ void UAsyncTaskGameplayTagAddedRemoved::EndTask()
 	}
 
 	SetReadyToDestroy();
-	MarkPendingKill();
+	MarkAsGarbage();
 }
 
 void UAsyncTaskGameplayTagAddedRemoved::TagChanged(const FGameplayTag Tag, int32 NewCount)

Source/GASShooter/Private/Characters/Abilities/AttributeSets/GSAttributeSetBase.cpp
@@ -144,7 +144,7 @@ void UGSAttributeSetBase::PostGameplayEffectExecute(const FGameplayEffectModCall
 					{
 						FGameplayTagContainer DamageNumberTags;
 
-						if (Data.EffectSpec.DynamicAssetTags.HasTag(HeadShotTag))
+						if (Data.EffectSpec.GetDynamicAssetTags().HasTag(HeadShotTag))
 						{
 							DamageNumberTags.AddTagFast(HeadShotTag);
 						}

commit 4200866fd5f534a95f05d7e6d6400112ddecd952

Fix inverted condition blocking task registration
The bug was introduced in commit
https://github.com/tranek/GASShooter/commit/a901592

Source/GASShooter/Private/Characters/Abilities/AbilityTasks/GSAT_WaitTargetDataUsingActor.cpp
@@ -205,7 +205,7 @@ void UGSAT_WaitTargetDataUsingActor::FinalizeTargetActor() const
 
 void UGSAT_WaitTargetDataUsingActor::RegisterTargetDataCallbacks()
 {
-	if (!ensure(!IsValid(this)))
+	if (!ensure(IsValid(this)))
 	{
 		return;
 	}